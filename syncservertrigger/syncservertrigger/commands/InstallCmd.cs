17541565bef1b9958dc36745a00a90aebcdecaf2
Sergio Luis Para <sergio@sergioluis.es>
2018-01-02 13:00:19 +0000

    Completed the installation of the necessary triggers. I have yet to write the dstServer in the configuration, but I'm taking care of the tedious parts related to the cm.exe interaction first.

--- syncservertrigger/syncservertrigger/commands/InstallCmd.cs
+++ syncservertrigger/syncservertrigger/commands/InstallCmd.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 
-using Codice.CmdRunner;
-
 namespace Codice.SyncServerTrigger.Commands
 {
     internal class InstallCmd : ICmd
@@ -15,23 +13,98 @@ namespace Codice.SyncServerTrigger.Commands
             if (args.Length != 3)
             {
                 Console.Error.WriteLine(HELP);
-                return;
+                Environment.Exit(1);
             }
 
             string srcServer = args[1];
             string dstServer = args[2];
 
+            Console.WriteLine(
+                "Installing the necessary triggers on server {1}:{0}" +
+                "* after-checkin{0}" +
+                "* after-replicationwrite{0}" +
+                "* after-mklabel{0}" +
+                "* after-chattvalue",
+                Environment.NewLine,
+                srcServer);
+
             string executablePath = Utils.GetAssemblyLocation();
+
+            Console.WriteLine(
+                "Using syncservertrigger located at {0}.",
+                executablePath);
+
+            InstallTrigger(
+                "after-checkin", "sync afterci", executablePath, srcServer);
+
+            InstallTrigger(
+                "after-replicationwrite", "sync-afterreplica", executablePath, srcServer);
+
+            InstallTrigger(
+                "after-mklabel", "sync aftermklabel", executablePath, srcServer);
+
+            InstallTrigger(
+                "after-chattvalue", "sync afterchattval", executablePath, srcServer);
+
+            Console.WriteLine(
+                "Triggers successfully installed! Check 'cm listtriggers --server={0}'.",
+                srcServer);
+        }
+
+        static void InstallTrigger(
+            string triggerType,
+            string triggerName,
+            string executablePath,
+            string server)
+        {
+            int result;
+            string stdOut, stdErr;
+
+            string cmdLine = GetCommandLine(
+                triggerType, triggerName, executablePath, server);
+
+            Console.WriteLine("Installing '{0}' trigger.", triggerType);
+
+            result = CmdRunner.CmdRunner.ExecuteCommandWithResult(
+                cmdLine,
+                Environment.CurrentDirectory,
+                out stdOut,
+                out stdErr,
+                false);
+
+            if (result == 0)
+                return;
+
+            WriteError(
+                cmdLine,
+                string.IsNullOrEmpty(stdOut) ? string.Empty : stdOut,
+                string.IsNullOrEmpty(stdErr) ? string.Empty : stdErr);
+
+            Environment.Exit(1);
+        }
+
+        static void WriteError(string commandLine, string output, string error)
+        {
+            Console.Error.WriteLine(
+                "Installing a trigger failed.{0}" +
+                "Command line: '{1}'{0}" +
+                "cm stdout: {2}{0}" +
+                "cm stderr: {3}",
+                Environment.NewLine,
+                commandLine,
+                output,
+                error);
         }
 
         static string GetCommandLine(
-            string triggerType, string triggerName, string executablePath, string[] args)
+            string triggerType, string triggerName, string executablePath, string server)
         {
-            return string.Format("cm maketrigger {0} \"{1}\" \"{2} {3}\"",
+            return string.Format(
+                "cm maketrigger {0} \"{1}\" \"{2} trigger {0}\" --server={3}",
                 triggerType,
                 triggerName,
                 executablePath,
-                string.Join(" ", args));
+                server);
         }
 
         const string HELP =
