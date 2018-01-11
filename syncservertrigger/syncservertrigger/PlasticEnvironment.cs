abc3f2a48cca8d2892c66048cccce0775b2fd804
Sergio Luis Para <sergio@sergioluis.es>
2018-01-11 11:50:40 +0100

    Added the PlasticEnvironment class to easily access the environment variables that the Plastic SCM server sets up before trigger execution.

---
+++ syncservertrigger/syncservertrigger/PlasticEnvironment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Codice.SyncServerTrigger
+{
+    internal static class PlasticEnvironment
+    {
+        internal static string PlasticChangeset
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("PLASTIC_CHANGESET");
+            }
+        }
+
+        internal static string PlasticBranch
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("PLASTIC_BRANCH");
+            }
+        }
+
+        internal static string PlasticLabelName
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("PLASTIC_LABEL_NAME");
+            }
+        }
+
+        internal static string PlasticRepositoryName
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("PLASTIC_REPOSITORY_NAME");
+            }
+        }
+    }
+}
