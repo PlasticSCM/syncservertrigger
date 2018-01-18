ac653ecf736d213fa50e6371629a21d4366883b8
Sergio Luis Para <sergio@sergioluis.es>
2018-01-18 11:22:22 +0100

    Added a model to represent the pending replicas, and a way to generate them based on the data we have (destination servers and repository mappings).

---
+++ syncservertrigger/syncservertrigger/models/Replica.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codice.SyncServerTrigger.Models
+{
+    internal class Replica
+    {
+        internal string SrcBranch { get; private set; }
+        internal string SrcRepo { get; private set; }
+        internal string SrcServer { get; private set; }
+        internal string DstRepo { get; private set; }
+        internal string DstServer { get; private set; }
+
+        internal Replica(
+            string srcBranch,
+            string srcRepo,
+            string srcServer,
+            string dstRepo,
+            string dstServer)
+        {
+            SrcBranch = srcBranch;
+            SrcRepo = srcRepo;
+            SrcServer = srcServer;
+            DstRepo = dstRepo;
+            DstServer = dstServer;
+        }
+
+        internal List<Replica> BuildPendingReplicas(
+            string srcBranch,
+            string srcRepo,
+            string srcServer,
+            List<string> filteredRepos,
+            List<string> dstServers,
+            List<RepoMapping> repoMappings)
+        {
+            List<Replica> result = new List<Replica>();
+
+            if (filteredRepos.Contains(srcRepo))
+                return result;
+
+            foreach (string server in dstServers)
+            {
+                result.Add(new Replica(
+                    srcBranch,
+                    srcRepo,
+                    srcServer,
+                    GetDstRepo(srcRepo, server, repoMappings),
+                    server));
+            }
+
+            return result;
+        }
+
+        static string GetDstRepo(
+            string srcRepo, string dstServer, List<RepoMapping> repoMappings)
+        {
+            foreach (RepoMapping mapping in repoMappings)
+            {
+                if (!dstServer.Equals(
+                    mapping.DstServer,
+                    StringComparison.InvariantCultureIgnoreCase))
+                {
+                    continue;
+                }
+
+                if (!srcRepo.Equals(
+                    mapping.SrcRepo,
+                    StringComparison.InvariantCultureIgnoreCase))
+                {
+                    continue;
+                }
+
+                return mapping.DstRepo;
+            }
+
+            return srcRepo;
+        }
+    }
+}
